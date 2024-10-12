8ddd42a203d1917e05262fd0cbe0de9af35c80de
Yeipy28 <117322606+Yeipy28@users.noreply.github.com>
2024-10-12 18:50:02 -0500

    Primer Commit

---
+++ Modeleps/src/main/java/EPS/Modeleps/EpsModelApplication.java
@@ -0,0 +1,13 @@
+package EPS.Modeleps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpsModelApplication {
+
+	
+	public static void main(String[] args) {
+		SpringApplication.run(EpsModelApplication.class, args);
+	}
+}
